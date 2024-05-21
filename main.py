42c3e79052e2015601a0714740f4f70542908dc4
jaykumarpatel04 <12271585@cqumail.com>
2024-05-21 15:47:39 +1000

    commit

--- main.py
+++ main.py
@@ -1,1 +1,17 @@
-print("Hello")
+
+print("Hello,world")
+
+A = input("Enter the Number:")
+B = input("Enter the Number:")
+
+C = int(A) + int(B)
+D =  int(A) - int(B)
+E =  int(A) * int(B)
+F =  int(A) / int(B)
+G =  int(A) % int(B)
+
+print(C)
+print(D)
+print(E)
+print(F)
+print(G)
