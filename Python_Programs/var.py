d780e9741179b0edf235794cf3ec4fe6815da31b
jaykumarpatel04 <12271585@cqumail.com>
2024-05-22 19:29:55 +1000

    update varfile

---
+++ Python_Programs/var.py
@@ -0,0 +1,39 @@
+print("variables!")
+
+## Crearing Variables
+
+# x = "John"
+# y = 5
+
+# print(x)
+# print(y)
+
+
+## Casting 
+
+# A = str(3)     # A = "3"
+# B = int(2)     # B = "2"
+# C = float(3)   # C = "3.0"
+# print(A,B,C)
+
+
+## Type function
+
+# print(type(x))  # String
+# print(type(y))  # int
+# print(type(C))  # float
+
+# same varaible But Different Numbers
+#J = 10
+#J = 25
+
+#print(J) # The output is 25 It update 10 to 25 
+
+# A="Patel Jay Kumar"
+# print("My Name is",A)
+
+
+
+
+
+
