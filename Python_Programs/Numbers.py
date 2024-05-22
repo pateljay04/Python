da84fe82bf88a7d6aeebae5c793f37cc32efbc66
jaykumarpatel04 <12271585@cqumail.com>
2024-05-22 18:40:12 +1000

    Numbers

---
+++ Python_Programs/Numbers.py
@@ -0,0 +1,23 @@
+
+print("Hello,world!")
+
+
+Num1 = input("Enter the Num1:") 
+Num2 = input("Enter the Num2:")
+
+A = int(Num1)+int(Num2)
+
+B = int(Num1)-int(Num2)
+
+C = int(Num1)*int(Num2)
+
+D = int(Num1)/int(Num2)
+
+E = int(Num1)%int(Num2)
+
+print("Addition of Two Numbers is:",A)
+print("Subtraction of Two Numbers is:",B)
+print("Multiplication of Two Numbers is:",C)
+print("Division of Two Numbers is:",D)
+print("Modulo(Remainder) of Two Numbers is:",E)
+
