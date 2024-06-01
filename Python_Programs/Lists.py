1e71a22b7a318ce3b10b5ddafbd3c3bb48c0eb35
jaykumarpatel04 <12271585@cqumail.com>
2024-06-01 15:00:52 +1000

    commit

---
+++ Python_Programs/Lists.py
@@ -0,0 +1,82 @@
+# thislist = ["apple", "banana", "cherry","mango"]
+# print(len(thislist))
+
+# Mylist = ["BIke","car","Mobile"]
+# print(len(Mylist))
+
+# thislist = list(("apple", "banana", "cherry"))
+# print(thislist)
+
+# print(Mylist[0])
+# print(Mylist[-1])
+
+
+# thislist = ["apple", "banana", "cherry", "orange", "kiwi", "melon", "mango"]
+# print(thislist[2:4])  #This will return the items from position 2 to 5.
+
+#                        #Remember that the first item is position 0,
+#                          #and note that the item in position 5 is NOT included
+
+# print(thislist[2:5])
+
+
+# thislist = ["apple", "banana", "cherry"]
+# if "apple" in thislist:
+#   print("Yes, 'apple' is in the fruits list")
+
+# if "cherry" in thislist:
+#   print("yes")
+
+
+# ## Change List items
+
+# thislist = ["apple", "banana", "cherry"]
+# thislist[1] = "blackcurrant"
+# thislist[2] = "Mango"
+
+# print(thislist)
+
+# thislist = ["apple", "banana", "cherry", "orange", "kiwi", "mango"]
+
+# thislist[1:3] = ["blackcurrant", "watermelon"]
+
+# print(thislist)
+
+# thislist = ["apple", "banana", "cherry"]
+
+# thislist[1:2] = ["blackcurrant", "watermelon"]
+
+# print(thislist)
+
+# thislist = ["apple", "banana", "cherry"]
+
+# thislist.insert(1, "watermelon")
+
+# print(thislist) 
+
+# thislist = ["apple", "banana", "cherry"]
+# tropical = ["mango", "pineapple", "papaya"]
+
+# tropical.extend(thislist)
+
+# print(tropical)
+
+
+# thislist = ["apple", "banana", "cherry"]
+# for y in thislist:
+#   print(y)
+
+
+
+# thislist = ["apple", "banana", "cherry"]
+# for i in range(len(thislist)):
+#   print(thislist[i])
+
+
+thislist = ["apple", "banana", "cherry"]
+i = 0
+while i < len(thislist):
+  print(thislist[i])
+  i = i + 1
+
+
