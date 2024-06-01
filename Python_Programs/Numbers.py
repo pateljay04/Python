e350021c9f900f82f0592d5c3c3f70f4e4a03730
jaykumarpatel04 <12271585@cqumail.com>
2024-06-01 16:24:46 +1000

    Added Student Report

--- Python_Programs/Numbers.py
+++ Python_Programs/Numbers.py
@@ -1,23 +1,68 @@
 
-print("Hello,world!")
+# print("Hello,world!")
 
 
-Num1 = input("Enter the Num1:") 
-Num2 = input("Enter the Num2:")
+# Num1 = input("Enter the Num1:") 
+# Num2 = input("Enter the Num2:")
 
-A = int(Num1)+int(Num2)
+# A = int(Num1)+int(Num2)
 
-B = int(Num1)-int(Num2)
+# B = int(Num1)-int(Num2)
 
-C = int(Num1)*int(Num2)
+# C = int(Num1)*int(Num2)
 
-D = int(Num1)/int(Num2)
+# D = int(Num1)/int(Num2)
 
-E = int(Num1)%int(Num2)
+# E = int(Num1)%int(Num2)
 
-print("Addition of Two Numbers is:",A)
-print("Subtraction of Two Numbers is:",B)
-print("Multiplication of Two Numbers is:",C)
-print("Division of Two Numbers is:",D)
-print("Modulo(Remainder) of Two Numbers is:",E)
+# print("Addition of Two Numbers is:",A)
+# print("Subtraction of Two Numbers is:",B)
+# print("Multiplication of Two Numbers is:",C)
+# print("Division of Two Numbers is:",D)
+# print("Modulo(Remainder) of Two Numbers is:",E)
+
+
+Student = input("Enter the Student Name: ")
+
+Student_Rollno = int(input("Enter the Student Rollno: "))
+
+Sub1 = int(input("Enter the Marks for Telugu Subject: "))
+
+Sub2 = int(input("Enter the Marks for Hindi Subject: "))
+
+Sub3 = int(input("Enter the Marks for English Subject: "))
+
+Sub4 = int(input("Enter the Marks for Maths Subject: "))
+
+Sub5 = int(input("Enter the Marks for Science Subject: "))
+
+Sub6 = int(input("Enter the Marks for Social Subject: "))
+
+Total_marks = Sub1+Sub2+Sub3+Sub4+Sub5+Sub6
+
+Percentage = Total_marks/600*100
+print("Student Name:",Student)
+print("The Total marks of the Student is:",Total_marks)
+print("The Student Percentage is:",Percentage)
+
+if (Percentage>=90):
+    print("The Student secured with Grade o")
+    
+elif(Percentage>=80):
+    print("The Student secured with Grade A")
+elif(Percentage>=70):
+    print("The Student secured withGrade B")
+elif(Percentage>=60):
+    print("The Student secured with Grade C")
+elif(Percentage>=50):
+    print("The Student secured with Grade D")
+elif(Percentage>=40):
+    print("The Student secured with Grade e")
+else:
+    print("Grade F is fail")
+
+
+    
+
+
 
