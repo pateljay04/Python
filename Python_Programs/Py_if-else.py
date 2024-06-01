c379b2f14d80d119d9bb78ce7db14062e816358f
jaykumarpatel04 <12271585@cqumail.com>
2024-06-01 20:11:00 +1000

    commit

--- Python_Programs/Py_if-else.py
+++ Python_Programs/Py_if-else.py
@@ -1,14 +1,84 @@
 print("Python If-else Statements")
 
 
-A = int(input("Enter your age: "))
+# A = int(input("Enter your age: "))
 
-if A>=18:
-    print("yes,You are eligible to vote")
-else:
-    print(" No,You are not eligible to vote")
+# if A>=18:
+#     print("yes,You are eligible to vote")
+# else:
+#     print(" No,You are not eligible to vote")
 
 
-    
+# Apple_Price = 150
+
+# print("The Apple price is:",Apple_Price)
+
+# Budget = int(input("Enter the Budget amount: "))
+# if  Budget - Apple_Price >= 50:
+#     print("Alexa, you can add 1kg of Apples")
+# elif Budget - Apple_Price > 30:
+#     print("Alexa, its okay you can add  Apples")
+# else:
+#    print("Alexa, don't Add apples")
+
+# A =  Budget - Apple_Price
+# print("The remaining amount is:",A)
+
+# a = 10
+# b = 100
+
+# if  b>a :
+#     print("B IS GREATER THAN A")
+# elif a==b:
+#     print("A IS EQUAL TO B")
+# else:
+#     print("A IS GREATER THAN B")
+
+## And Operator 
+
+# a = 200
+# b = 33
+# c = 100
+# if a > b and c > a:
+#   print("Both conditions are True")
+# else:
+#   print("Both condition are not True")
+
+## Or operator
+
+# a = 100
+# b = 33
+# c = 500
+# if a > b or a > c:
+#   print("At least one of the conditions is True")
+
+# a = 2
+# b = 10
+# c = 2
+# if a==c or b==c:
+#     print("yes") 
+
+
+
+# not key word 
+
+# a = 9
+# b = 10
+# if not a > b:
+#   print("a is NOT greater than b")
+
+
+# Nested if 
+# x = 10
+
+# if x > 10:
+#   print("Above ten,")
+#   if x > 20:
+#     print("and also above 20!")
+#   else:
+#     print("but not above 20.")
+
+
+
 
 
