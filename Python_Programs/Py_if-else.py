8cb1d9ae2cdb175502015c1c762206792c185927
jaykumarpatel04 <12271585@cqumail.com>
2024-06-01 15:14:09 +1000

    commit

---
+++ Python_Programs/Py_if-else.py
@@ -0,0 +1,11 @@
+print("Python If-else Statements")
+
+
+A = int(input("Enter your age: "))
+
+if A>18:
+    print("yes,You are eligible to vote")
+else:
+    print(" No,You are not eligible to vote")
+
+
